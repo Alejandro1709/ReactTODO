a8e9543e46ddfee2e56c742ee035e0f8a4eed161
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-29 14:27:11 -0500

    DONE: APP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 import { v4 as uuidv4 } from 'uuid';
+import About from './components/pages/About';
 import './App.css';
 
 class App extends Component {
@@ -55,19 +57,30 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <div className="app__container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <main>
-            <Todos
-              todos={this.state.todos}
-              markComplete={this.markComplete}
-              delTodo={this.delTodo}
+      <Router>
+        <div className="app">
+          <div className="app__container">
+            <Header />
+            <Route
+              path="/"
+              exact
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <main>
+                    <Todos
+                      todos={this.state.todos}
+                      markComplete={this.markComplete}
+                      delTodo={this.delTodo}
+                    />
+                  </main>
+                </React.Fragment>
+              )}
             />
-          </main>
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
