638f6028acee0cb5f894baeb8e5abc9ce1529c69
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-11-29 12:24:58 -0500

    ADD: State toggling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,11 +3,28 @@ import PropTypes from 'prop-types';
 import './TodoItem.css';
 
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+    };
+  };
+
   render() {
+    const { id, title } = this.props.todo;
+
     return (
-      <div className="todos">
+      <div className="todos" style={this.getStyle()}>
         <div className="todo">
-          <h3>{this.props.todo.title}</h3>
+          <h3>
+            <input
+              type="checkbox"
+              onChange={this.props.markComplete.bind(this, id)}
+            />{' '}
+            {title}
+          </h3>
         </div>
       </div>
     );
